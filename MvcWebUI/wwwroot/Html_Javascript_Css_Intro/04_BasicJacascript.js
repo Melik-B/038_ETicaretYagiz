5c2d1152ff7d2f3e82d54c5f5a185c8f9d986376
melik baykal <melikbilgeadam@outlook.com>
2022-03-06 19:37:32 +0300

    V1

---
+++ MvcWebUI/wwwroot/Html_Javascript_Css_Intro/04_BasicJacascript.js
@@ -0,0 +1,38 @@
+﻿function ucgenDoldur(tersten = false) {
+    var sonuc;
+    /*var sonucArray = []*/
+    var sonucArray = new Array();
+    for (var i = 0; i < 5; i++) {
+        sonuc = "";
+        for (var j = 0; j <= i; j++) {
+            sonuc += "*";
+        }
+        sonucArray[i] = sonuc;
+    }
+    sonuc = "";
+    if (!tersten) {
+        for (var i = 0; i < sonucArray.length; i++) {
+            sonuc += sonucArray[i] + "\n";
+        }
+    }
+    else {
+        for (var i = sonucArray.length - 1; i >= 0; i--) {
+            sonuc += sonucArray[i] + "\n";
+        }
+    }
+    //console.log(sonuc);
+    return sonuc;
+}
+
+function onLoadIleDoldur() {
+    var mytextarea = document.getElementById("mytextareaid");
+    mytextarea.value = ucgenDoldur();
+}
+
+var terstenGlobal = true;
+
+function onClickIleDoldur() {
+    var mytextarea = document.getElementById("mytextareaid");
+    mytextarea.value = ucgenDoldur(terstenGlobal);
+    terstenGlobal = !terstenGlobal;
+}
